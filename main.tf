449bb66be214cfa9a800532f6008f46177f90fe7
terraformaccount <89585737+terraformaccount@users.noreply.github.com>
2021-09-21 15:08:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,10 +30,8 @@ resource "azurerm_public_ip" "hcmxexample" {
   resource_group_name = data.azurerm_resource_group.hcmxexample.name
   location            = var.location
   allocation_method   = "Dynamic"
-  domain_name_label   = var.vm_name
-  tags = {
-    environment = var.tag
-  }
+  domain_name_label   = var.domain_name_label
+ 
 }
 
 data "azurerm_network_security_group" "hcmxexample" {
@@ -42,36 +40,12 @@ data "azurerm_network_security_group" "hcmxexample" {
   resource_group_name = data.azurerm_resource_group.hcmxexample.name
   }
   
-  #resource "azurerm_network_security_rule" "hcmxexample" {
-  #name                        = "ssh"
-  #priority                    = 100
-  #direction                   = "Inbound"
-  #access                      = "Allow"
-  #protocol                    = "Tcp"
-  #source_port_range           = "*"
-  #destination_port_range      = "22"
-  #source_address_prefix       = "*"
-  #destination_address_prefix  = "*"
-  #resource_group_name         = data.azurerm_resource_group.hcmxexample.name
-  #network_security_group_name = azurerm_network_security_group.hcmxexample.name
-#}
 
-# resource "azurerm_network_ddos_protection_plan" "hcmxexample" {
-# name                = var.name
-# location            = var.location
-# resource_group_name = azurerm_resource_group.hcmxexample.name
-# }
 
 data "azurerm_virtual_network" "hcmxexample" {
   name                = var.virtual_network
   #location            = data.azurerm_network_security_group.hcmxexample.location
   resource_group_name = data.azurerm_resource_group.hcmxexample.name
- # address_space       = ["10.0.0.0/16"]
-
-  # ddos_protection_plan {
-  #  id     = azurerm_network_ddos_protection_plan.hcmxexample.id
-  #  enable = true
-  #}
 
 }
 
@@ -79,7 +53,6 @@ data "azurerm_subnet" "hcmxexample" {
   name                 = var.subnet
   resource_group_name  = data.azurerm_resource_group.hcmxexample.name
   virtual_network_name = data.azurerm_virtual_network.hcmxexample.name
-  #address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "hcmxexample" {
@@ -96,10 +69,7 @@ resource "azurerm_network_interface" "hcmxexample" {
   }
 
 
-#resource "azurerm_private_dns_zone" "hcmxexample" {
-#  name                = "hcmxexample.com"
-#  resource_group_name = data.azurerm_resource_group.hcmxexample.name
-#}
+
   
  resource "azurerm_linux_virtual_machine" "hcmxexample" {
   count = var.os_type=="linux" ? 1 : 0
@@ -113,9 +83,7 @@ resource "azurerm_network_interface" "hcmxexample" {
   network_interface_ids = [
     azurerm_network_interface.hcmxexample.id,
   ]
-  tags = {
-    environment = var.tag
-  }
+ 
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = var.type_of_storage
@@ -140,9 +108,7 @@ resource "azurerm_windows_virtual_machine" "hcmxexample" {
   network_interface_ids = [
     azurerm_network_interface.hcmxexample.id,
   ]
-  tags = {
-    environment = var.tag
-  }
+ 
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = var.type_of_storage
@@ -159,7 +125,6 @@ resource "azurerm_windows_virtual_machine" "hcmxexample" {
 resource "azurerm_managed_disk" "hcmxexample" {
   name                 = "${var.vm_name}-disk"
   location             = var.location
-  #resource_group_name  = var.os_type=="linux" ? azurerm_linux_virtual_machine.hcmxexample[0].name : azurerm_windows_virtual_machine.hcmxexample[0].name
   resource_group_name = data.azurerm_resource_group.hcmxexample.name
   storage_account_type = var.type_of_storage
   create_option        = "Empty"
@@ -176,7 +141,7 @@ resource "azurerm_virtual_machine_data_disk_attachment" "hcmxexample" {
 data "azurerm_public_ip" "hcmxexample" {
   name                = var.vm_name
   resource_group_name = var.os_type=="linux" ? azurerm_linux_virtual_machine.hcmxexample[0].resource_group_name : azurerm_windows_virtual_machine.hcmxexample[0].resource_group_name
- #resource_group_name = data.azurerm_resource_group.hcmxexample.name
+
 }
 
 output "public_ip_address" {
@@ -186,7 +151,7 @@ output "public_ip_address" {
 data "azurerm_network_interface" "hcmxexample" {
   name                = var.vm_name
   resource_group_name = var.os_type=="linux" ? azurerm_linux_virtual_machine.hcmxexample[0].resource_group_name : azurerm_windows_virtual_machine.hcmxexample[0].resource_group_name
-  #resource_group_name = var.resource_group_name
+
 }
 
 output "network_interface_name" {
@@ -201,50 +166,24 @@ output "primary_dns_name" {
   value = data.azurerm_public_ip.hcmxexample.fqdn
 }
 
-# output "resource_group_name" {
-#   value = var.os_type=="linux" ? azurerm_linux_virtual_machine.hcmxexample[0].name : azurerm_windows_virtual_machine.hcmxexample[0].name
-# }
 
-# output "network_security_group_name" {
-#   value = data.azurerm_network_security_group.hcmxexample.name
-# }
-
-# output "azurerm_subnet" {
-#   value = data.azurerm_subnet.hcmxexample.name
-# }
-
-# output "virtual_network_name" {
-#   value = data.azurerm_subnet.hcmxexample.virtual_network_name
-# }
 
 output "azurerm_network_interface" {
   value = azurerm_network_interface.hcmxexample.name
 }
 
-# output "size" {
-#   value = var.os_type=="linux" ? azurerm_linux_virtual_machine.hcmxexample[0].size : azurerm_windows_virtual_machine.hcmxexample[0].size
-# }
 
 output "virtual_machine_id" {
   value = var.os_type=="linux" ? azurerm_linux_virtual_machine.hcmxexample[0].virtual_machine_id : azurerm_windows_virtual_machine.hcmxexample[0].virtual_machine_id
 }
 
-output "mac_address" {
-  value = azurerm_network_interface.hcmxexample.mac_address
-}
 
 output "cloud_instance_id" {
   value = var.os_type=="linux" ? azurerm_linux_virtual_machine.hcmxexample[0].id : azurerm_windows_virtual_machine.hcmxexample[0].id
 }
 
-#output "azurerm_private_dns_zone" {
-#  value = azurerm_private_dns_zone.hcmxexample.name
-#}
 
 output "data_disk_name" {
   value = azurerm_managed_disk.hcmxexample.name
 }
-
-
-
 
